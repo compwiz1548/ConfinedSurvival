4460625fccf46cc6a1c8d111aa5912bc82586b64
compwiz1548 <compwiz1548@gmail.com>
2014-09-10 08:49:35 -0400

    Removed any nametags mobs spawn with.

--- src/main/java/com/compwiz1548/ConfinedSurvival/CSListener.java
+++ src/main/java/com/compwiz1548/ConfinedSurvival/CSListener.java
@@ -3,11 +3,13 @@ package com.compwiz1548.ConfinedSurvival;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class CSListener implements Listener {
+
+    //Stop a player from entering the nether before the correct number of Mutant Zombies have been killed.
     @EventHandler(priority = EventPriority.MONITOR)
     public void onEnterNetherPortal(PlayerPortalEvent event) {
         if (event.getCause() == PlayerTeleportEvent.TeleportCause.NETHER_PORTAL) {
@@ -18,10 +20,9 @@ public class CSListener implements Listener {
         }
     }
 
+    //Remove names on mobs when they are spawned.
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onKill(EntityDeathEvent event) {
-        String entityName = event.getEntity().toString();
-        if (entityName.contains("MutantZombie"))
-            Config.incrementKilled();
+    public void onSpawn(CreatureSpawnEvent event) {
+        event.getEntity().setCustomNameVisible(false);
     }
 }
